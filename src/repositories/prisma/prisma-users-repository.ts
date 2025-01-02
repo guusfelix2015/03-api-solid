339ae08685c180871d0cfe639ee53ac838e00ffa
guusfelix2015 <gusfelix2015@gmail.com>
2025-01-02 14:57:57 -0300

    caso de uso do perfil

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,18 @@
 import { prisma } from '@/lib/prisma'
 import { UsersRepository } from '@/repositories/users-repository'
-import { Prisma } from '@prisma/client'
+import { Prisma, User } from '@prisma/client'
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
